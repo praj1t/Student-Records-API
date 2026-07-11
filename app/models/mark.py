1cfc6367aa35516bb375aa76ece8465dcc4267f6
praj1t <138615650+praj1t@users.noreply.github.com>
2026-07-12 01:43:36 +0800

    Set up SQLAlchemy models and Alembic migrations

--- app/models/mark.py
+++ app/models/mark.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy import Column,Integer, String, DateTime, ForeignKey, Float, UniqueConstraint
 from sqlalchemy.sql import func
 from app.database import Base
 from sqlalchemy.orm import relationship
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 
 class Mark(Base):
     __tablename__ = "marks"
+    __table_args__ = (UniqueConstraint("student_id","subject_id", name="unique_student_subject_mark"),)
     id = Column(Integer, primary_key=True, index=True)
     student_id = Column(Integer,ForeignKey("students.id"), nullable=False)
     subject_id = Column(Integer,ForeignKey("subjects.id"), nullable=False)
